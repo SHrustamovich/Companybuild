bf8d0d97a282a8fc18044f7638bbbd5abd1208a6
SHrustamovich <87068197+SHrustamovich@users.noreply.github.com>
2022-03-09 11:25:59 +0500

    two

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const connection = {
     connectionString: 'postgres://postgres:shohruh0949@localhost:5432/build',
     connectionElString: 'postgres://ngalvtjt:jomUDr853o8wG4pE9rRrZC6WB28liwaJ@jelani.db.elephantsql.com/ngalvtjt'
 }
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 9000
 module.exports = {
     connection,
     PORT
